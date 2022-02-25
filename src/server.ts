b6e3b2e6f81657a97f3c36ab07f944f7e443bbf7
fasyed-innominds <fasyed@innominds.com>
2022-02-25 10:44:45 +0530

    added env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { App } from './app';
 require('dotenv').config();
-console.log("env",process.env);
+console.log("env",process.env.WSO2_URL);
 const app = new App(3000);
 app.use();
 app.listen();
