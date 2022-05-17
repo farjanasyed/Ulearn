263133fb289d49fc848cfffaad85c299d354857b
farjana-wirewheel <farjana.syed@wirewheel.io>
2022-05-17 15:35:28 +0530

    Added auth controller

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,10 +1,8 @@
 import AuthService from '../services/Auth';
 import { Request, Response } from 'express';
-import qs from 'qs';
-
 class AuthContrller {
     public async getAuthToken(req: Request, res: Response) {
-
+        console.log("Request::GetToken",req.body);
         req.body["grant_type"] = "password"
         req.body["scope"]= "openid"
         req.body["username"] = req.body.userName;
@@ -40,6 +38,9 @@ class AuthContrller {
                 })
         })
     }
+
+
+   
     public async getUserInfo(req: Request, res: Response) {
         AuthService.getUserInfo(req.headers.authorization).then((result:any) => {
             console.log("GETUSER::Error",result);
@@ -47,12 +48,17 @@ class AuthContrller {
             let userResponse = {};
 
             if(result.status == 200){
-
+                 let rolesList = [];
+                result.data?.roles.forEach((role:string)=>{
+                    if(!role.includes("everyone")){
+                        rolesList.push(role.substring(role.indexOf('/')+1));
+                    }
+                })
                 userResponse["email"] = result.data?.emails ? result.data?.emails[0] :"" ;
                 userResponse["phoneNumber"] = result.data?.phoneNumbers ? result.data?.phoneNumbers[0].value: ""
                 userResponse["firstName"] = result.data?.name?.givenName ? result.data?.name?.givenName : "";
                 userResponse["familyName"] = result.data?.name?.familyName ? result.data?.name.familyName : "";
-                userResponse["roles"] = result.data?.roles;
+                userResponse["roles"] = rolesList
      
              }
             res.status(200).send({
